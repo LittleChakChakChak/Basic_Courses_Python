8b0eef32819e23c99a63121f40c903623d7ea6cf
littleChakChakChak <kovtundmitriy11@gmail.com>
2021-09-23 19:59:10 +0300

    Завершение задания

--- СoursesPython0/example04.py
+++ СoursesPython0/example04.py
@@ -2,9 +2,11 @@ import random
 from itertools import count
 from itertools import cycle
 from functools import reduce
+from math import factorial
+
 
 # 1 функция расчета заработной платы сотрудника -------------------------------------------------------
-"""
+
 def payroll(salary):
     return salary[0] * salary[1] + salary[2]
 
@@ -13,10 +15,10 @@ salary = [int(input('Введите зарплату в час: ')),
           int(input('Введите часы работы: ')),
           int(input('Введите премию: '))]
 print(f'Зарплата: {payroll(salary)}')
-"""
+
 
 # 2 Вывод большего элемента в сравнение с предыдущем -----------------------------------------------
-"""
+
 def parsing(myArray):
     newArray = []
     for i in range(len(myArray)):
@@ -34,10 +36,10 @@ while i < 10:
     i += 1
 print(f'Массив(список) изначально {myArray}')
 print(f'Вот такой массив(список) получился{parsing(myArray)}')
-"""
+
 # 3  Для чисел от 20 до 240 найти кратные 20 или 21 одной строкой -----------------------------------------------
-"""
-print(f'{[i for i in range(20,241) if i % 20 == 0 or i % 21 == 0]}')
+
+print(f'{[i for i in range(20, 241) if i % 20 == 0 or i % 21 == 0]}')
 
 # 4 Список без повторений  -----------------------------------------------
 myList = []
@@ -45,25 +47,24 @@ for i in range(10):
     myList.append(random.randint(0, 100))
 print(f'Список изначально: {myList}')
 print(f'Список без повторений: {set(myList)}')
-"""
+
 # 5 Список без повторений  -----------------------------------------------
-"""
+
 myList = []
 for i in range(10):
     myList.append(random.randrange(99, 1001, 2))
 print(f'Вывод списка: {myList}')
 print(f'Вывод суммы: {reduce(lambda x, y: x + y, myList)}')
-"""
 
 # 6  Реализование два скрипта для циклирования списка -----------------------------------------------
 # 6.1 итератор, генерирующий целые числа, начиная с указанного
-"""
+
 begin = int(input('Введите с какого числа начинать: '))
 for i in count(begin):
     print('Вывод: ', i)
     if i > 9:
         break
-"""
+
 # 6.2 итератор, повторяющий элементы некоторого списка, определенного заранее
 myList = []
 for i in range(5):
@@ -74,4 +75,20 @@ for i in cycle(myList):
     check += 1
     print(i)
     if check > 20:
+        break
+
+
+# 7  Реализование два скрипта для циклирования списка -----------------------------------------------
+def func():
+    for fact in count(1):
+        yield factorial(fact)
+
+
+n = int(input('Введите факториал: '))
+gen = func()
+for el in gen:
+    if n > 0:
+        print(f'Факториал числа = {el}!')
+        n -= 1
+    else:
         break
