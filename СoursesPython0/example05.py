c642f37cfe826fb8f05783806a1a7e5a38ebe834
littleChakChakChak <kovtundmitriy11@gmail.com>
2021-09-30 00:38:32 +0300

    Добавление файлов и заданий

--- СoursesPython0/example05.py
+++ СoursesPython0/example05.py
@@ -1,5 +1,5 @@
 # 1 заполнить файл текстовым значение ------------------------------------------
-
+"""
 file1 = open("files/file_1", "w")
 while True:
     str = input('Введите что добавить в файл: ')
@@ -9,7 +9,7 @@ while True:
         break
 file1.close()
 
-"""
+
 # 2 прочтение файла (кол-во строк и символов в строке) ------------------------------------------
 
 with open('files/file_2', 'r') as file2:
@@ -20,20 +20,49 @@ with open('files/file_2', 'r') as file2:
         char = len(line) - 1
         sumChar += char
         print(f'В {lines} строке было {char} символов')
-    file2.close()
 print(f'Всего было: {lines} строк и {sumChar} символов без пробела')
 """
-""""
+
 # 3 прочтение файла (кол-во строк и символов в строке) ------------------------------------------
+"""
 with open('files/file_3', 'r') as file3:
-    lines = 0
-    employee = []
-    staff = []
+    staff = {}
+    sumSalary = 0
     for line in file3:
-        lines += 1
-        employee = line.split()
-        staff = employee.append()
+        key, value = line.split()
+        staff[key] = value
+        if int(value) < 20000:
+            print(f'Фамилия: {key}, зарплата: {value}')
+        sumSalary += int(value)
+    print(f'Средняя зарплата по сотрудникам: {sumSalary / len(staff)}')
+    print(f'Список всех сотрудников: {staff}')
 
 
-    file3.close()
-print(staff)
+# 4 перевод текста из файла в другой файл ------------------------------------------
+
+with open('files/file_4', 'r') as file4:
+    file4_1 = open('files/file_4_1.txt', 'w')
+    rus = {1: 'Один', 2: 'Два', 3: 'Три', 4: 'Четыре'}
+    for line in file4:
+        value, key = line.split(' — ')
+        file4_1.write(f'{rus[int(key[0])]} — {key[0]}\n')
+    file4_1.close()
+print('Переведен')
+
+# 5 сумма из файла ------------------------------------------
+
+with open('files/file_5', 'r') as file5:
+    summer = []
+    summ = 0
+    summer = file5.read().split()
+    for i in summer:
+        summ += int(i)
+print(f'Сумма чисел = {summ}')
+"""
+# 6 Запись предметов  ------------------------------------------
+
+with open('files/file_6.txt', 'w') as file6:
+    # сделать ввод по циклу
+    # если пробел то отмена
+    # если пробел в занятие то прочерк
+    # по оканчанию сделать список
